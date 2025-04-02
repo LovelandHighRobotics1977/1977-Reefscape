30c88f526a9bf4a00aa6b899911412ac7d145c8f
TheTree243 <157831493+TheTree243@users.noreply.github.com>
2025-04-02 11:10:53 -0600

    Delete src directory

--- src/main/include/RobotBase/Parameters/RobotParameters.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef _ROBOT_PARAMETERS_H
-#define _ROBOT_PARAMETERS_H
-
-#include "AutoParameters.hpp"
-#include "TeleopParameters.hpp"
-#include "MechParameters.hpp"
-
-#endif
--- src/main/include/Subsystems/Devices/Devices.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef _AUX_DEVICES_H
-#define _AUX_DEVICES_H
-
-#include "Field.hpp"
-#include "Gyro.hpp"
-#include "PDH.hpp"
-
-#endif	// _AUX_DEVICES_H
